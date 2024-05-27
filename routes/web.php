2dd697c5d4c90bc2f5130c0cf0f69c9f58ab98a8
Emmanuel Ayeh <52609990+Aemmi@users.noreply.github.com>
2024-05-27 09:33:05 -1200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\SliderController;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -57,3 +63,48 @@ Route::get('/blog', function () {
 Route::get('/contact', function () {
     return view('contact');
 }) ->name('contact');
+
+Route::controller(PostController::class)->group(function () {
+    Route::get('/b/{slug}', 'viewPost')->name('home.viewBlogPost');
+    Route::get('/blog', 'blog')->name('home.blog');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::controller(CategoryController::class)->group(function () {
+        Route::post('/admin/addCategory', 'addCategory')->name('admin.addCategory');
+        Route::get('/admin/categories', 'categories')->name('admin.categories');
+        Route::get('/admin/delete-category/{id}', 'destroy')->name('admin.deleteCategory');
+        Route::get('/admin/get-category/{id}', 'getCategory');
+        Route::post('/admin/update-category', 'update')->name('admin.updateCategory');
+    });
+    Route::controller(PostController::class)->group(function () {
+        Route::get('/admin/posts', 'index')->name('admin.posts');
+        Route::get('/admin/editPost/{post_id}', 'edit')->name('admin.editPost');
+        Route::post('/admin/editPost/{post_id}', 'update');
+        Route::post('/admin/savePost', 'store')->name('admin.savePost');
+    });
+    Route::controller(SettingController::class)->group(function () {
+        Route::get('/admin/settings', 'index')->name('admin.settings');
+        Route::post('/admin/save-settings', 'store')->name('admin.saveSettings');
+    });
+    Route::controller(PageController::class)->group(function () {
+        Route::get('/admin/pages', 'index')->name('admin.pages');
+        Route::post('/admin/save-page', 'store')->name('admin.savePage');
+        Route::get('/admin/editPage/{page_id}', 'edit')->name('admin.editPage');
+        Route::post('/admin/update-page', 'update')->name('admin.updatePage');
+    });
+
+    Route::controller(ImageController::class)->group(function () {
+        Route::get('/admin/images', 'index')->name('admin.images');
+        Route::get('/admin/delete-image/{file}', 'destroy');
+        Route::post('/admin/uploadImage', 'store')->name('admin.uploadImage');
+    });
+
+    Route::controller(SliderController::class)->group(function () {
+        Route::get('/admin/sliders', 'index')->name('admin.sliders');
+        Route::post('/admin/save-slider', 'store')->name('admin.saveSlider');
+        Route::get('/admin/delete-slider/{id}', 'destroy')->name('admin.deleteSlider');
+    });
+
+});
